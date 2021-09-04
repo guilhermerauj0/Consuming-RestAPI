43e51609e5425e275def6fc71c8d6ed4730d731d
arthu <arthurguilhermesb@gmail.com>
2021-09-04 20:10:42 -0300

    Gramatic correction and alteration in classpath for gradle

--- app/src/main/java/com/coldrosemob/consumingrestapi/MainActivity.java
+++ app/src/main/java/com/coldrosemob/consumingrestapi/MainActivity.java
@@ -1,11 +1,12 @@
 package com.coldrosemob.consumingrestapi;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
